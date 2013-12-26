2e859de201d127d80b296ec988975bdbd3de7851
Angelo Nutile <angelo.nutile@gmail.com>
2013-12-26 17:15:26 +0100

    Added Bootstrap

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-/* CMS.JS */
+/* app.js */
+/* Modular Template Node.js Express Jade */
 
-var port = process.env.PORT || 3000;
-var express = require('express');
+var appConfig = require('./appConfig.json');
+
+var port = process.env.PORT || appConfig.app.devPort;
+var express = require('express');	
+var path = require('path');
+
 var app = express();
 
 /* Your Modules */
@@ -9,6 +14,12 @@ var login = require('./lib/login');
 var registration = require ('./lib/registration');
 var users = require('./lib/users');
 var contents = require ('./lib/contents');
+
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/_views');
+app.use(express.static(path.join(__dirname, 'public')));
+
+var brand = ' Modular Template Node.js Express Jade '
 
 app.use(login);
 app.use(registration);
